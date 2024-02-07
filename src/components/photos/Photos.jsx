d87240c73b33a2adadd0ab576b3bff4d47a13fc7
AlvaroRK <adriano.rk06@gmail.com>
2024-02-06 21:29:37 -0300

    fetch de photos agregado

---
+++ src/components/photos/Photos.jsx
@@ -0,0 +1,21 @@
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
+
+const Photos = ({ photosC }) => {
+  return (
+    <>
+      {photosC.map((photo, i) => {
+        return (
+          <Card key={i}>
+            <CardHeader>
+              <h1 className="font-bold text-center">{photo.title}</h1>
+            </CardHeader>
+            <CardBody>
+              <img src={photo.url} alt="" />
+            </CardBody>
+          </Card>
+        );
+      })}
+    </>
+  );
+};
+export default Photos;
--- src/components/photos/PhotosContainer.jsx
+++ src/components/photos/PhotosContainer.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
+import Photos from "./Photos";
+
+
+
 const PhotosContainer = () => {
+  const [api, setApi] = useState([]);
+
+  useEffect(() => {
+    consumeData();
+  }, []);
+
+
+  const min = 1
+  const max = 50
+  const maxItem = Math.floor(Math.random() * (max - min + 1) + min)
+
+  const consumeData = async () => {
+    const data = await fetch("https://jsonplaceholder.typicode.com/photos");
+    const dataJson = await data.json();
+    setApi(dataJson.slice(0, maxItem));
+  };
   return (
-    <div>PhotosContainer</div>
+    <main className="grid grid-cols-4 gap-8">
+        <Photos photosC={api}></Photos>
+    </main>
   )
 }
 export default PhotosContainer
